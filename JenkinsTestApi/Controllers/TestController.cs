57cb1e03975ce7817ef2155d7a9f1a5737991e1a
cankirism <cankiri.bilsis@saglik.gov.tr>
2022-10-11 13:44:20 +0300

    Add project files.

---
+++ JenkinsTestApi/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsTestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+
+        public string Get()
+        {
+            return "hello";
+        }
+    }
+}
